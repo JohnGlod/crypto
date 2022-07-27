56fa07a95aa3556f6e62eba63a4b3a0b5d2bf498
JohnGlod <EvgeniiDolgopolov@outlook.com>
2022-07-27 20:40:32 +0500

    create Transactions contract

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
+/* import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
 };
 
+export default config;
+ */
+
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers'; // types
+import { HardhatUserConfig } from 'hardhat/config';
+const config: HardhatUserConfig = {
+  solidity: '0.8.0',
+};
 export default config;
